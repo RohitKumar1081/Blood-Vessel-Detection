a854c39f3fc198d5491562e3cd9aa29dd397a691
Rohit Kumar <rohitkumarthegreat1@gmail.com>
2022-07-17 15:03:45 +0530

    main

---
+++ main.py
@@ -0,0 +1,77 @@
+import cv2
+import numpy as np
+import streamlit as st
+
+def display_img(img,cmap=None):
+    fig = plt.figure(figsize=(10,8))
+    ax = fig.add_subplot(111)
+    ax.imshow(img,cmap='gray')
+
+def extract_bv(image):
+
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    contrast_enhanced_green_fundus = clahe.apply(image)
+
+    r1 = cv2.morphologyEx(contrast_enhanced_green_fundus, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5)), iterations = 1)
+    R1 = cv2.morphologyEx(r1, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5)), iterations = 1)
+    r2 = cv2.morphologyEx(R1, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(11,11)), iterations = 1)
+    R2 = cv2.morphologyEx(r2, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(11,11)), iterations = 1)
+    r3 = cv2.morphologyEx(R2, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(23,23)), iterations = 1)
+    R3 = cv2.morphologyEx(r3, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(23,23)), iterations = 1)    
+    f4 = cv2.subtract(R3,contrast_enhanced_green_fundus)
+    f5 = clahe.apply(f4)
+
+    ret,f6 = cv2.threshold(f5,15,255,cv2.THRESH_BINARY) 
+    mask = np.ones(f5.shape[:2], dtype="uint8") * 255
+
+    contours, hierarchy = cv2.findContours(f6.copy(),cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in contours:
+        if cv2.contourArea(cnt) <= 200:
+            cv2.drawContours(mask, [cnt], -1, 0, -1)
+    im = cv2.bitwise_and(f5, f5, mask=mask)
+    ret,fin = cv2.threshold(im,15,255,cv2.THRESH_BINARY_INV)
+    newfin = cv2.erode(fin, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3)), iterations=1)
+
+    fundus_eroded = cv2.bitwise_not(newfin) 
+    xmask = np.ones(fundus.shape[:2], dtype="uint8") * 255
+    xcontours, xhierarchy = cv2.findContours(fundus_eroded.copy(),cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in xcontours:
+        shape = "unidentified"
+        peri = cv2.arcLength(cnt, True)
+        approx = cv2.approxPolyDP(cnt, 0.04 * peri, False) 
+        if len(approx) > 4 and cv2.contourArea(cnt) <= 3000 and cv2.contourArea(cnt) >= 100:
+            shape = "circle"
+        else:
+            shape = "veins"
+        if(shape=="circle"):
+            cv2.drawContours(xmask, [cnt], -1, 0, -1)
+
+    finimage = cv2.bitwise_and(fundus_eroded,fundus_eroded,mask=xmask)
+    blood_vessels = cv2.bitwise_not(finimage)
+    return blood_vessels    
+
+if __name__ == "__main__":
+
+    st.title("Blood Vessel Detection")
+
+    image_upload = st.sidebar.file_uploader("Load your fundus image here")
+    if image_upload is not None:
+        img = cv2.imread(image_upload)
+    else:
+        img = cv2.imread("example.tif")
+
+    img = cv2.resize(img,(800,615))
+
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    img1=cv2.addWeighted(img,4, cv2.GaussianBlur(img, (0,0) , 20) ,-4 ,128)
+    img1 = img1[:, :, 1]
+
+    draw_landmark_button = st.button('Detect Vessel')
+        
+    if draw_landmark_button:
+        result = extract_bv(img1)
+        st.header("Image with vessel detection")
+        st.image(result)
+    else:
+        st.header("Original Image")
+        st.image(img)
